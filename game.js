d6690ead053556ab4f22037ba7fa913ab4e0643c
niboo <moonevening8@gmail.com>
2021-07-06 20:46:44 +0900

    random game

---
+++ game.js
@@ -0,0 +1,31 @@
+/** @format */
+
+const allForm = document.querySelector("#enter");
+const genInput = document.querySelector("#max");
+const guessInput = document.querySelector("#guess input");
+const btn = document.querySelector("#guess button");
+const youChosen = document.querySelector("#choose span:first-child");
+const machineChosen = document.querySelector("#choose span:last-child");
+const result = document.querySelector("#result");
+
+function genRandom(min, max) {
+	const ranNum = Math.floor(Math.random() * (max - min + 1) + min);
+	return ranNum;
+}
+
+function buttonClick(event) {
+	event.preventDefault();
+	let max = document.getElementById("max").value;
+	const guessInputNumber = Number(guessInput.value);
+	const genInputNumber = Number(genInput.value);
+	const randomNumber = genRandom(0, max);
+	youChosen.innerText = `You chose:${guessInputNumber}`;
+	machineChosen.innerText = `, machine chose: ${randomNumber}`;
+	if (guessInputNumber === randomNumber) {
+		result.innerText = "You won!";
+	} else {
+		result.innerText = "You lost!";
+	}
+}
+
+allForm.addEventListener("submit", buttonClick);
